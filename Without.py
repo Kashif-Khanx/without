407a840c2797f865bdc5c8fe1fb6a5cde2198eda
Kashif-Khanx <105439063+Kashif-Khanx@users.noreply.github.com>
2022-05-13 15:22:52 +0500

    Update Without.py

--- Without.py
+++ Without.py
@@ -11,30 +11,22 @@ d607e3ad1336453e9a54~~KRS==
 
 Alina
 3db0a9f8f0564bce9564~~KRS==
-
 Sat
 e5f0363b6a7c413ca95f~~KRS==
-
 Suno
 a1e4df529cda40758eaf~~KRS==
-
 Mahr
 a7a2b3f47fba4c958a60~~KRS==
-
 Junaid
 4ca70926ecf6442eb390~~KRS==
-
 Kiran
 151e41e2752c4e3f95f1~~KRS==
-
 Sabir
 e79b768ff11847c4bea4~~KRS==
-
 Zin
 edee5fafdd01445a8e8c~~KRS==
-
 Vala
 0b15d3fdc56741b5bf29~~KRS==
 
-
 b8d09cfabaa54ce1a495~~KRS==
+754ccf615e5b497a8270~~KRS==
